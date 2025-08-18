bcfb5d822aa3d885d154d64cab505e0d3c52ff22
VISHAK <49461325+vishakvijayaraghavan@users.noreply.github.com>
2025-08-18 23:29:24 +0530

    s3 file update

---
+++ s3/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region        = "us-east-1"
+bucket_name       = "test-bucket"
+environment       = "platform"
---
+++ s3/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "select thecaws region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "bucket name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
