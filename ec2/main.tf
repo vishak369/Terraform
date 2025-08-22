d8128b47c8ccf3504fd2052ac0428ff7163e974f
VISHAK <49461325+vishakvijayaraghavan@users.noreply.github.com>
2025-08-22 23:57:15 +0530

    security group update

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,6 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+resource "aws_security_group" "ec2-sg" {
+  name        = "ec2-sg"
+  description = "ssh access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_ami" "amazon_linux" {
     most_recent = true
     owners = [ "amazon" ]
