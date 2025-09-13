bca8fc729e94d575f578bdb479f586bbc9e874f7
VISHAK <49461325+vishakvijayaraghavan@users.noreply.github.com>
2025-09-13 23:58:20 +0530

    aws_elasticache2

--- elasticache/main.tf
+++ elasticache/main.tf
@@ -27,3 +27,14 @@ resource "aws_elasticache_parameter_group" "cluster1_pg" {
     value = "yes"
   }
 }
+
+resource "aws_elasticache_replication_group" "example" {
+  automatic_failover_enabled  = true
+  preferred_cache_cluster_azs = ["us-west-2a"]
+  replication_group_id        = "tf-rep-group-1"
+  description                 = "elasticachen group"
+  node_type                   = "cache.m4.large"
+  num_cache_clusters          = 2
+  parameter_group_name        = "default.redis3.2"
+  port                        = 6379
+}
