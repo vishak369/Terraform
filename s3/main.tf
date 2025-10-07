baa94b0a8c2dd35c83ef2d505cd1b52c30944ca6
VISHAK <49461325+vishak369@users.noreply.github.com>
2025-10-07 23:42:31 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -2,11 +2,20 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "bucket1" {
   bucket = var.bucket_name
 
   tags = {
     Name        = var.bucket_name
     Environment = var.environment
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
